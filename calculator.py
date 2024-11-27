14ee6947ee8fcba9a5d31f0e1689dbc4b485a0c6
knoxyys <stirlingthebean@gmail.com>
2024-11-27 12:18:03 +1100

    cleared up everything

--- calculator.py
+++ calculator.py
@@ -1,8 +1,14 @@
-x = float(input("x = "))
-y = float(input("y = ")) # blah blah gets input
+def main():
+    x = float(input("x = "))
+    y = float(input("y = ")) # blah blah gets input
 
-add = round(x + y)
-divide = round(x / y, 2) # the 2 specifies the amount of decimal points to round to
+    add = round(x + y)
+    divide = round(x / y, 2) # the 2 specifies the amount of decimal points to round to
 
-print(f"x + y = {add:,}") # colon is a separator for formatting, comma a builtin thousands separator i think?
-print(f"x / y = {divide:.2f}") # .2 specifies the decimal places (forces it, will fill with 0 if not needed), f specifies float
+    print(f"x + y = {add:,}") # colon is a separator for formatting, comma a builtin thousands separator i think?
+    print(f"x / y = {divide:.2f}") # .2 specifies the decimal places (forces it, will fill with 0 if not needed), f specifies float
+
+def square(n):
+    return n * n
+
+main()
